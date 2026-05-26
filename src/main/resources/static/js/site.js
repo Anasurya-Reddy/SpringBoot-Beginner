17ad8c81dff08a9af79af42d505f23632662efe1
Anasurya Reddy <sathiansuryareddy@gmail.com>
2026-05-26 12:29:16 +0530

    added cards and toggle function

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,1 +1,21 @@
-console.log("Hello from JS");
+let name = "John Doe";
+let gender = "Male";
+let image = "img/john.png";
+
+function toggle() {
+    // Toggle
+    if (gender === "Male") {
+        gender = "Female";
+        name = "Jane Doe";
+        image = "img/jane.png";
+    } else {
+        gender = "Male";
+        name = "John Doe";
+        image = "img/john.png";
+    }
+
+    // Update the DOM
+    document.getElementById("image").src = image;
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("gender").innerHTML = gender;
+}
